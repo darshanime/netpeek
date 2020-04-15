1fb898a880baf94f466c20acf88b689fb238294c
darshanime <deathbullet@gmail.com>
2020-04-15 15:10:47 +0530

    create bpf programs from args

--- main.go
+++ main.go
@@ -17,31 +17,38 @@ import (
 
 var iface = flag.String("i", "en0", "interface to read packets from")
 var useCui = flag.Bool("cui", false, "set CUI mode")
-var bpf = flag.String("bpf", "tcp", "bpf program")
-var appPort = flag.String("port", "80", "application http port")
+var sPort = flag.String("sport", "", "source port")
+var dPort = flag.String("dport", "", "destination port")
+var sHost = flag.String("shost", "", "source host port")
+var dHost = flag.String("dhost", "", "destination port")
+var protocol = flag.String("protocol", "http", "protocol to parse for")
 var quiet = flag.Bool("q", false, "quiet mode")
 
 func main() {
 	flag.Parse()
-	fmt.Printf("%s\n", pcap.Version())
-	fmt.Printf("iface %s\n", *iface)
-	fmt.Printf("useCui %t\n", *useCui)
-	fmt.Printf("bpf %s\n", *bpf)
-	fmt.Printf("appPort %s\n", *appPort)
-	fmt.Printf("quiet %t\n", *quiet)
+	fmt.Printf("pcap version: %s\n", pcap.Version())
+	fmt.Printf("iface: %s\n", *iface)
+	fmt.Printf("useCui: %t\n", *useCui)
+	fmt.Printf("sPort: %s\n", *sPort)
+	fmt.Printf("dPort: %s\n", *dPort)
+	fmt.Printf("sHost: %s\n", *sHost)
+	fmt.Printf("dHost: %s\n", *dHost)
+	fmt.Printf("protocol: %s\n", *protocol)
+	fmt.Printf("quiet: %t\n", *quiet)
+	fmt.Printf("bpf: %s\n", getBPFProgram())
 
 	handle, err := pcap.OpenLive(*iface, int32(65535), true, pcap.BlockForever)
 	if err != nil {
 		panic(fmt.Sprintf("cannot open %s interface for sniffing", *iface))
 	}
 	defer handle.Close()
-	err = handle.SetBPFFilter(*bpf)
+	err = handle.SetBPFFilter(getBPFProgram())
 	if err != nil {
 		panic("incorrect bpf program")
 	}
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	streamFactory := &stream.HTTPStreamFactory{UseCui: useCui, HTTPPort: appPort}
+	streamFactory := &stream.HTTPStreamFactory{UseCui: useCui, Protocol: protocol, Quiet: quiet}
 	streamPool := reassembly.NewStreamPool(streamFactory)
 	assembler := reassembly.NewAssembler(streamPool)
 
@@ -74,4 +81,54 @@ func main() {
 			assembler.FlushCloseOlderThan(time.Now().Add(time.Minute * -2))
 		}
 	}
+}
+
+func getBPFProgram() string {
+	flagMap := getFlagMap()
+	switch flagMap {
+	case "0000":
+		return "tcp"
+	case "0001":
+		return fmt.Sprintf("tcp and (dst host %s or src host %s)", *dHost, *dHost)
+	case "0010":
+		return fmt.Sprintf("tcp and (src host %s or dst host %s)", *sHost, *sHost)
+	case "0100":
+		return fmt.Sprintf("tcp and (dst port %s or src port %s)", *dPort, *dPort)
+	case "1000":
+		return fmt.Sprintf("tcp and (src port %s or dst port %s)", *sPort, *sPort)
+	case "1001":
+		return fmt.Sprintf("tcp and ((src port %s and dst host %s) or (dst port %s and src host %s))", *sHost, *dHost, *sHost, *dHost)
+	case "1010":
+		return fmt.Sprintf("tcp and ((src port %s and src host %s) or (dst port %s and dst host %s))", *sPort, *sHost, *sPort, *sHost)
+	case "0011":
+		return fmt.Sprintf("tcp and ((src host %s and dst host %s) or (dst host %s and src host %s))", *sHost, *dHost, *sHost, *dHost)
+	case "0101":
+		return fmt.Sprintf("tcp and ((dst port %s and dst host %s) or (src port %s and src host %s))", *dPort, *dHost, *dPort, *dHost)
+	case "0110":
+		return fmt.Sprintf("tcp and ((dst port %s and src host %s) or (src port %s and dst host %s))", *dPort, *sHost, *dPort, *sHost)
+	case "1100":
+		return fmt.Sprintf("tcp and ((src port %s and dst port %s) or (dst port %s and src port %s))", *sPort, *dPort, *sPort, *dPort)
+	case "0111":
+		return fmt.Sprintf("tcp and ((dst port %s and src host %s and dst host %s) or (src port %s and dst host %s and src host %s))", *dPort, *sHost, *dHost, *dPort, *sHost, *dHost)
+	case "1011":
+		return fmt.Sprintf("tcp and ((src port %s and src host %s and dst host %s) or (dst port %s and dst host %s and src host %s))", *sPort, *sHost, *dHost, *sPort, *sHost, *dHost)
+	case "1101":
+		return fmt.Sprintf("tcp and ((src port %s and dst port %s and dst host %s) or (dst port %s and src port %s and src host %s))", *sPort, *dPort, *dHost, *sPort, *dPort, *dHost)
+	case "1110":
+		return fmt.Sprintf("tcp and ((src port %s and dst port %s and src host %s) or (dst port %s and src port %s and dst host %s))", *sPort, *dPort, *sHost, *sPort, *dPort, *sHost)
+	case "1111":
+		return fmt.Sprintf("tcp and ((src port %s and dst port %s and src host %s and dst host %s) or (dst port %s and src port %s and dst host %s and src host %s))", *sPort, *dPort, *sHost, *dHost, *sPort, *dPort, *sHost, *dHost)
+	default:
+		panic("could not build bpf program")
+	}
+}
+
+func getFlagMap() string {
+	isPresent := func(opt *string) string {
+		if *opt != "" {
+			return "1"
+		}
+		return "0"
+	}
+	return isPresent(sPort) + isPresent(dPort) + isPresent(sHost) + isPresent(dHost)
 }
