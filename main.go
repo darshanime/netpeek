d96b8bed1cfe1882b9f468f86462056afbc56408
darshanime <deathbullet@gmail.com>
2020-04-18 19:43:24 +0530

    allow useCui only for http

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ var sHost = flag.String("shost", "", "source host port")
 var dHost = flag.String("dhost", "", "destination port")
 var protocol = flag.String("protocol", "http", "protocol to parse for")
 var verbose = flag.Bool("v", false, "verbose mode")
+var bpf = flag.String("bpf", "", "custom bpf program")
 
 func main() {
 	flag.Parse()
@@ -36,15 +37,24 @@ func main() {
 	logger.Printf("verbose: %t", *verbose)
 	logger.Printf("bpf: %s", getBPFProgram())
 
+	if *useCui && *protocol != "http" {
+		panic("cui mode can only be used with http protocol")
+	}
+
 	handle, err := pcap.OpenLive(*iface, int32(65535), true, pcap.BlockForever)
 	if err != nil {
 		panic(fmt.Sprintf("cannot open %s interface for sniffing", *iface))
 	}
 	defer handle.Close()
-	err = handle.SetBPFFilter(getBPFProgram())
-	if err != nil {
-		panic("incorrect bpf program")
+
+	if *bpf == "" {
+		*bpf = getBPFProgram()
 	}
+
+	if err := handle.SetBPFFilter(*bpf); err != nil {
+		panic(fmt.Sprintf("incorrect bpf program: %s", *bpf))
+	}
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	streamFactory := &stream.HTTPStreamFactory{UseCui: useCui, Protocol: protocol, Logger: logger}
@@ -68,7 +78,7 @@ func main() {
 				return
 			}
 			if packet.NetworkLayer() == nil || packet.TransportLayer() == nil || packet.TransportLayer().LayerType() != layers.LayerTypeTCP {
-				logger.Printf("Unusable packet")
+				logger.Printf("unusable packet")
 				continue
 			}
 			tcp := packet.TransportLayer().(*layers.TCP)
@@ -81,54 +91,4 @@ func main() {
 			assembler.FlushCloseOlderThan(time.Now().Add(time.Minute * -2))
 		}
 	}
-}
-
-func getBPFProgram() string {
-	flagMap := getFlagMap()
-	switch flagMap {
-	case "0000":
-		return "tcp"
-	case "0001":
-		return fmt.Sprintf("tcp and (dst host %s or src host %s)", *dHost, *dHost)
-	case "0010":
-		return fmt.Sprintf("tcp and (src host %s or dst host %s)", *sHost, *sHost)
-	case "0100":
-		return fmt.Sprintf("tcp and (dst port %s or src port %s)", *dPort, *dPort)
-	case "1000":
-		return fmt.Sprintf("tcp and (src port %s or dst port %s)", *sPort, *sPort)
-	case "1001":
-		return fmt.Sprintf("tcp and ((src port %s and dst host %s) or (dst port %s and src host %s))", *sHost, *dHost, *sHost, *dHost)
-	case "1010":
-		return fmt.Sprintf("tcp and ((src port %s and src host %s) or (dst port %s and dst host %s))", *sPort, *sHost, *sPort, *sHost)
-	case "0011":
-		return fmt.Sprintf("tcp and ((src host %s and dst host %s) or (dst host %s and src host %s))", *sHost, *dHost, *sHost, *dHost)
-	case "0101":
-		return fmt.Sprintf("tcp and ((dst port %s and dst host %s) or (src port %s and src host %s))", *dPort, *dHost, *dPort, *dHost)
-	case "0110":
-		return fmt.Sprintf("tcp and ((dst port %s and src host %s) or (src port %s and dst host %s))", *dPort, *sHost, *dPort, *sHost)
-	case "1100":
-		return fmt.Sprintf("tcp and ((src port %s and dst port %s) or (dst port %s and src port %s))", *sPort, *dPort, *sPort, *dPort)
-	case "0111":
-		return fmt.Sprintf("tcp and ((dst port %s and src host %s and dst host %s) or (src port %s and dst host %s and src host %s))", *dPort, *sHost, *dHost, *dPort, *sHost, *dHost)
-	case "1011":
-		return fmt.Sprintf("tcp and ((src port %s and src host %s and dst host %s) or (dst port %s and dst host %s and src host %s))", *sPort, *sHost, *dHost, *sPort, *sHost, *dHost)
-	case "1101":
-		return fmt.Sprintf("tcp and ((src port %s and dst port %s and dst host %s) or (dst port %s and src port %s and src host %s))", *sPort, *dPort, *dHost, *sPort, *dPort, *dHost)
-	case "1110":
-		return fmt.Sprintf("tcp and ((src port %s and dst port %s and src host %s) or (dst port %s and src port %s and dst host %s))", *sPort, *dPort, *sHost, *sPort, *dPort, *sHost)
-	case "1111":
-		return fmt.Sprintf("tcp and ((src port %s and dst port %s and src host %s and dst host %s) or (dst port %s and src port %s and dst host %s and src host %s))", *sPort, *dPort, *sHost, *dHost, *sPort, *dPort, *sHost, *dHost)
-	default:
-		panic("could not build bpf program")
-	}
-}
-
-func getFlagMap() string {
-	isPresent := func(opt *string) string {
-		if *opt != "" {
-			return "1"
-		}
-		return "0"
-	}
-	return isPresent(sPort) + isPresent(dPort) + isPresent(sHost) + isPresent(dHost)
 }
